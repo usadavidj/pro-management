e3084ceb896f1f7a5355472d463f34d51bd82f40
usadavidj <skidavidj@gmail.com>
2018-08-09 09:53:37 -0600

    setup with routes and header

---
+++ src/components/auth/signup.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+class Signup extends Component {
+    render() {
+        return (
+            <div className='sign-in'>
+                ...signup
+            </div>
+        )
+    }
+}
+
+
+export default Signup;
